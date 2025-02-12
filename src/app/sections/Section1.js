3088861a495142a597bca9b8231a6ad53ba24a82
ANDY Hormazabal <andy@andy.cl>
2025-02-11 22:14:26 -0300

    add section and font ok

--- src/app/sections/Section1.js
+++ src/app/sections/Section1.js
@@ -6,10 +6,10 @@ export default function Section1() {
   return (
     <section className="bg-[#004632]">
           <div className="max-w-6xl mx-auto p-8">
-            <h1 className="text-4xl font-bold text-gray-800 mb-4">antenna</h1>
-            <h2 className="text-2xl font-semibold text-gray-700 mb-6">
-              fundación
-            </h2>
+            <div>
+              <img src="/img/logos/antenna_celeste.png" className="w-[7rem]" />
+              <img src="/img/logos/mecenas_celeste.png"  className="w-20"  />
+            </div>
             <div className="rounded-lg flex w-full">
               <div className="w-[68%]">
                 <h3 className="font-black font-mona mb-4 text-7xl font-bold text-[#93d3e1]">
